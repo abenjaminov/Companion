cd9ed27cf626c23e59fc1d56f5dec811ced9a7f9
abenjaminov <asafb@prodropsgame.com>
2023-04-11 15:49:33 +0300

    Episode 3

--- Assets/_Scripts/MapGenerator.cs
+++ Assets/_Scripts/MapGenerator.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -10,16 +12,39 @@ namespace _Scripts
         public int mapSize;
         public float noiseScale;
         public MapDisplay mapDisplay;
+        public List<Octave> octaves;
+        [Range(.001f,1)] public float persistance;
+        public float lacunarity;
+        public int seed;
+        public Vector2 offset;
 
         public void GenerateMap()
         {
             var noiseMap = Noise.GenerateNoiseMap(new GenerateNoiseMapOptions()
             {
                 Scale = this.noiseScale,
-                Size = this.mapSize
+                Size = this.mapSize,
+                Persistance = this.persistance,
+                Lacunarity = this.lacunarity,
+                Octaves = this.octaves,
+                Offset = this.offset,
+                Seed = this.seed
             });
             
             this.mapDisplay.DrawNoiseMap(noiseMap);
+        }
+
+        private void OnValidate()
+        {
+            if (this.mapSize < 1)
+            {
+                this.mapSize = 1;
+            }
+
+            if (this.lacunarity < 1)
+            {
+                this.lacunarity = 1;
+            }
         }
     }
 }
--- Assets/_Scripts/Noise.cs
+++ Assets/_Scripts/Noise.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 namespace _Scripts
 {
+    [Serializable]
     public struct Octave
     {
-        public int DetailLevel;
+        public int DetailsLevel;
     }
+    
     public struct GenerateNoiseMapOptions
     {
         public int Size;
@@ -14,6 +18,8 @@ namespace _Scripts
         public List<Octave> Octaves;
         public float Persistance;
         public float Lacunarity;
+        public int Seed; // This option is so that we can get different and unique noise maps
+        public Vector2 Offset; // This options enables us to scroll through the Noise
     }
 
     public static class Noise
@@ -22,6 +28,20 @@ namespace _Scripts
         {
             var noiseMap = new float[options.Size, options.Size];
 
+            var prng = new Random(options.Seed);
+
+            var octaveOffsets = new List<Vector2>();
+            
+            // We want each octave to be sampled from a different location on the
+            // perlin noise
+            foreach (var octave in options.Octaves)
+            {
+                var offsetX = prng.Next(-100000, 100000) + options.Offset.x;
+                var offsetY = prng.Next(-100000, 100000) + options.Offset.y;
+                
+                octaveOffsets.Add(new Vector2(offsetX, offsetY));
+            }
+            
             // We use scale so that we dont get the same values for each noise map generated
             var scale = options.Scale;
 
@@ -32,6 +52,8 @@ namespace _Scripts
 
             var maxNoiseHeight = float.MinValue;
             var minNoiseHeight = float.MaxValue;
+
+            var center = new Vector2(options.Size / 2f, options.Size / 2f);
             
             for (var y = 0; y < options.Size; y++)
             {
@@ -39,18 +61,19 @@ namespace _Scripts
                 {
                     var noiseHeight = 0f;
                     
-                    foreach (var octave in options.Octaves)
+                    for (var i = 0; i < options.Octaves.Count; i++)
                     {
-                        var amplitude = Mathf.Pow(options.Persistance, octave.DetailLevel);
-                        var frequency = Mathf.Pow(options.Lacunarity, octave.DetailLevel);
+                        var octave = options.Octaves[i];
                         
-                        var sampleX = (x / scale) * frequency;
-                        var sampleY = (y / scale) * frequency;
+                        var amplitude = Mathf.Pow(options.Persistance, octave.DetailsLevel);
+                        var frequency = Mathf.Pow(options.Lacunarity, octave.DetailsLevel);
+                        
+                        var sampleX = (x - center.x) / scale * frequency + octaveOffsets[i].x;
+                        var sampleY = (y - center.y) / scale * frequency + octaveOffsets[i].y;;
 
-                        var perlinValue = Mathf.Lerp(-1,1, Mathf.PerlinNoise(sampleX, sampleY));
+                        var perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
 
-                        noiseHeight = perlinValue * amplitude;
-
+                        noiseHeight += perlinValue * amplitude;
                     }
 
                     if (noiseHeight > maxNoiseHeight)
@@ -66,6 +89,7 @@ namespace _Scripts
                 }
             }
 
+            // Normalize all the values of the noise map to be between 0.0 and 1.0
             for (var y = 0; y < options.Size; y++)
             {
                 for (var x = 0; x < options.Size; x++)
