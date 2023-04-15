4ee8a0e36d2f0c75455cc81e3155c47cb12dcfc8
abenjaminov <asafb@prodropsgame.com>
2023-04-15 21:31:41 +0300

    Player Controller

---
+++ Assets/_Scripts/Companion/CompanionAI.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
+
+namespace _Scripts.Companion
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class CompanionAI : MonoBehaviour
+    {
+        public Transform player;
+
+        private NavMeshAgent _agent;
+
+        private Vector3 _previousPosition;
+        private float _previousTime;
+
+        public float CurrentSpeed;
+
+        private Transform _companionTransform;
+        
+        private void Start()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+            _companionTransform = transform;
+            _previousPosition = _companionTransform.position;
+        }
+
+        private void Update()
+        {
+            _agent.SetDestination(player.position);
+
+            var distance = Mathf.Sqrt((transform.position - _previousPosition).sqrMagnitude);
+            CurrentSpeed = distance / Time.deltaTime;
+            
+            _previousPosition = _companionTransform.position;
+        }
+    }
+}
---
+++ Assets/_Scripts/Companion/CompanionStateMachine.cs
@@ -0,0 +1,9 @@
+using _Scripts.States;
+
+namespace _Scripts.Companion
+{
+    public class CompanionStateMachine
+    {
+        private StateMachine _stateMachine;
+    }
+}
