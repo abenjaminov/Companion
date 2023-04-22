8aed053961501a59f0f9892622c957b31037c9e8
Asaf Benjaminov <abenjaminov@gmail.com>
2023-04-23 00:22:56 +0300

    on weapon change

--- Assets/_Scripts/Player/PlayerGameState.cs
+++ Assets/_Scripts/Player/PlayerGameState.cs
@@ -1,4 +1,5 @@
-﻿using _Scripts.Player.Types;
+﻿using System;
+using _Scripts.Player.Types;
 using _Scripts.Systems.InputSystem;
 using UnityEngine;
 using UnityEngine.Events;
@@ -12,6 +13,16 @@ namespace _Scripts.Player
         public InputReader inputReader;
         
         public WeaponType CurrentWeaponType = WeaponType.None;
+
+        private void Start()
+        {
+            inputReader.OnToggleWeaponClickedEvent += OnToggleWeaponClickedEvent;
+        }
+
+        private void OnToggleWeaponClickedEvent()
+        {
+            SetWeaponType(CurrentWeaponType == WeaponType.None ? WeaponType.Rifle : WeaponType.None);
+        }
 
         public void SetWeaponType(WeaponType weaponType)
         {
